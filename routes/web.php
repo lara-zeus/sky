c929dcb05524e2a2459d46b8d6ec0b98f46153a6
Ashraf Monshi <wh7r.com@gmail.com>
2022-04-21 07:31:54 +0300

    configure prefix
    add configuration for pages and posts URL prefixes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaraZeus\Sky\Http\Livewire\Page;
+use LaraZeus\Sky\Http\Livewire\Post;
+use LaraZeus\Sky\Http\Livewire\Posts;
+use LaraZeus\Sky\Http\Livewire\Tags;
 
 Route::prefix(config('zeus-sky.path'))
     ->middleware(config('zeus-sky.middleware'))
     ->group(function () {
-        Route::get('/', \LaraZeus\Sky\Http\Livewire\Posts::class)->name('blogs');
-        Route::get('/{post:slug}', \LaraZeus\Sky\Http\Livewire\Post::class)->name('post');
-        Route::get('page/{slug}', \LaraZeus\Sky\Http\Livewire\Page::class)->name('page');
-        Route::get('{type}/{slug}', \LaraZeus\Sky\Http\Livewire\Tags::class)->name('tags');
+        Route::get('/', Posts::class)->name('blogs');
+        Route::get(config('zeus-sky.post_uri_prefix') . '/{post:slug}', Post::class)->name('post');
+        Route::get(config('zeus-sky.page_uri_prefix') . '/{slug}', Page::class)->name('page');
+        Route::get('{type}/{slug}', Tags::class)->name('tags');
 
         Route::get('passConf', function () {
-            session()->put(request('postID').'-'.request('password'), request('password'));
+            session()->put(request('postID') . '-' . request('password'), request('password'));
 
             return redirect()->back()->with('status', 'sorry, password incorrect!');
         });

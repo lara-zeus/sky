abff3c2b60536c932ff07652bc3daf14fc6be800
Ashraf Monshi <wh7r.com@gmail.com>
2022-04-28 21:28:23 +0300

    add trans

--- resources/views/themes/zeus/partial/empty.blade.php
+++ resources/views/themes/zeus/partial/empty.blade.php
@@ -1,4 +1,4 @@
 <div class="px-4 py-4 bg-gradient-to-br from-white via-white to-secondary-50 shadow-lg rounded-3xl hover:shadow-xl transition ease-in-out duration-300 flex gap-2 items-center justify-center w-full h-72 border border-gray-100">
     <x-iconpark-wavesleft-o class="h-10 w-10 text-secondary-600" />
-    <span class="underline decoration-wavy underline-offset-4 decoration-primary-600">No posts yet!</span>
+    <span class="underline decoration-wavy underline-offset-4 decoration-primary-600">{{ __('No posts yet') }}!</span>
 </div>

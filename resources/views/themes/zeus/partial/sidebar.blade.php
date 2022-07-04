25d56e7a31857ab0d0fdab906cc5cea8eaef4644
David Grosse <dgrosse@amvisor.com>
2022-07-04 11:32:45 +0200

    introduce full-text search

--- resources/views/themes/zeus/partial/sidebar.blade.php
+++ resources/views/themes/zeus/partial/sidebar.blade.php
@@ -1,4 +1,5 @@
 {{--@include($theme.'.partial.authors')--}}
+@include($theme.'.partial.sidebar.search')
 @include($theme.'.partial.sidebar.categories')
 @include($theme.'.partial.sidebar.recent')
 @include($theme.'.partial.sidebar.pages')
--- src/Http/Livewire/Posts.php
+++ src/Http/Livewire/Posts.php
@@ -2,6 +2,8 @@
 
 namespace LaraZeus\Sky\Http\Livewire;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 use LaraZeus\Sky\Models\Post;
 use LaraZeus\Sky\Models\Tag;
 use Livewire\Component;
@@ -10,28 +12,49 @@ class Posts extends Component
 {
     public function render()
     {
+        $search = strtolower(request()->get('search'));
+
+        $posts = Post::NotSticky();
+        $posts = $this->applySearch($posts, $search);
+        $posts = $posts
+            ->orderBy('published_at', 'desc')
+            ->get();
+
+        $pages = Post::page();
+        $pages = $this->applySearch($pages, $search);
+        $pages = $pages
+            ->orderBy('published_at', 'desc')
+            ->whereNull('parent_id')
+            ->get();
+
+        $recent = Post::posts()
+                    ->limit(config('zeus-sky.site_recent_count', 5))
+                    ->orderBy('published_at', 'desc')
+                    ->get();
+
         return view(app('theme').'.home')
             ->with([
-                'posts' => Post::NotSticky()
-                    ->orderBy('published_at', 'desc')
-                    ->get(),
-
-                'pages' => Post::page()
-                    ->orderBy('published_at', 'desc')
-                    ->whereNull('parent_id')
-                    ->get(),
-
-                'tags' => Tag::withCount('postsPublished')
-                    ->where('type', 'category')
-                    ->get(), // $this->tag->postsPublished
-
+                'posts'    => $posts,
+                'pages'    => $pages,
+                'recent'   => $recent,
+                'tags'     => Tag::withCount('postsPublished')->where('type', 'category')->get(),
                 'stickies' => Post::sticky()->get(),
-
-                'recent' => Post::posts()
-                    ->take(config('zeus-sky.site_recent_count', 5))
-                    ->orderBy('published_at', 'desc')
-                    ->get(),
             ])
             ->layout(config('zeus-sky.layout'));
+    }
+
+    private function applySearch(Builder $query, string $search): Builder
+    {
+        if ($search) {
+            return $query->where(function ($query) use ($search) {
+                foreach (['title', 'slug', 'content', 'description'] as $attribute) {
+                    $query->orWhere(DB::raw("lower($attribute)"), 'like', "%$search%");
+                }
+
+                return $query;
+            });
+        }
+
+        return $query;
     }
 }

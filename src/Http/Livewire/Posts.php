b621af474639077761dfcd89a7b137c90de95417
David Grosse <dgrosse@amvisor.com>
2022-07-05 13:06:54 +0200

    improve highlighting algorithm once more

--- src/Http/Livewire/Posts.php
+++ src/Http/Livewire/Posts.php
@@ -68,16 +68,29 @@ class Posts extends Component
             return $posts;
         }
 
-        $class = config('zeus-sky.search_result_highlight_css_class', 'highlight');
-
         foreach ($posts as $i => $post) {
-            $posts[$i]->content = str_ireplace(
-                $search,
-                sprintf('<span class="%s">%s</span>', $class, $search),
-                $post->content,
-            );
+            $posts[$i]->content = static::hl($post->content, [$search]);
         }
 
         return $posts;
+    }
+
+    /**
+     * Credits for this code goes to link below.
+     *
+     * @link https://stackoverflow.com/questions/8564578/php-search-text-highlight-function
+     */
+    public static function hl(string $inp, array $words): string
+    {
+        $class = config('zeus-sky.search_result_highlight_css_class', 'highlight');
+        $replace = array_flip(array_flip($words));
+        $pattern = [];
+
+        foreach ($replace as $k => $fword) {
+            $pattern[] = '/\b('.$fword.')(?!>)\b/i';
+            $replace[$k] = sprintf('<span class="%s">$1</span>', $class);
+        }
+
+        return preg_replace($pattern, $replace, $inp);
     }
 }

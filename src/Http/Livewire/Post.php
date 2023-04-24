85c95928c2b3dfb3e78f186d5b135b040a5f9b5b
Ash Monsh <wh7r.com@gmail.com>
2023-04-24 07:02:12 +0300

    run pint

--- src/Http/Livewire/Post.php
+++ src/Http/Livewire/Post.php
@@ -14,14 +14,14 @@ class Post extends Component
         $this->post = postModel::where('slug', $slug)->firstOrFail();
 
         if ($this->post->status !== 'publish') {
-            abort_if(!auth()->check(), 404);
+            abort_if(! auth()->check(), 404);
             abort_if($this->post->user_id !== auth()->user()->id, 401);
         }
     }
 
     public function render()
     {
-        if (!$this->post->getMedia('posts')->isEmpty()) {
+        if (! $this->post->getMedia('posts')->isEmpty()) {
             seo()->image($this->post->getFirstMediaUrl('posts'));
         }
 

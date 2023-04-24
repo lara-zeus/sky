ccdb92c4b2f1916f660e9c6cd3859b873ff6b262
Ash Monsh <wh7r.com@gmail.com>
2023-04-24 06:09:10 +0300

    add config for default featured image
    add config for default featured image
    add missing alt for images

--- resources/views/themes/zeus/partial/children-pages.blade.php
+++ resources/views/themes/zeus/partial/children-pages.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ route('page',$post->slug) }}">
     <div class="flex space-x-3 rtl:space-x-reverse">
         @if($post->image() !== null)
-            <img src="{{ $post->image() }}" class="h-6 w-6 shadow-md rounded-[2rem] rounded-bl-none z-0 object-cover"/>
+            <img alt="{{ $post->title }}" src="{{ $post->image() }}" class="h-6 w-6 shadow-md rounded-[2rem] rounded-bl-none z-0 object-cover"/>
         @endif
         <div class="w-full">
             <div class="flex items-center justify-between">

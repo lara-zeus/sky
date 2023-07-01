2244b8790b675650476c0baaa1530776ec556bcc
Ash Monsh <wh7r.com@gmail.com>
2023-07-01 10:12:46 +0300

    fix editor clases

--- src/Classes/TinyEditor.php
+++ src/Classes/TinyEditor.php
@@ -3,20 +3,29 @@
 namespace LaraZeus\Sky\Classes;
 
 use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Textarea;
 use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor as TinyEditorAlias;
 
 class TinyEditor implements ContentEditor
 {
     public static function component(): Component
     {
-        return TinyEditorAlias::make('content')
-            ->label(__('Post Content'))
-            ->showMenuBar()
-            ->required();
+        if (class_exists(TinyEditorAlias::class)) {
+            return TinyEditorAlias::make('content')
+                ->label(__('Post Content'))
+                ->showMenuBar()
+                ->required();
+        }
+
+        return Textarea::make('content')->required();
     }
 
     public static function render(string $content): string
     {
-        return html_entity_decode($content);
+        if (class_exists(TinyEditorAlias::class)) {
+            return html_entity_decode($content);
+        }
+
+        return $content;
     }
 }

fc5e43f710fb47dc7aaf16a30976cd18ee33582a
Ash Monsh <wh7r.com@gmail.com>
2023-07-25 11:08:48 +0300

    fix if sky disabled

--- src/SkyServiceProvider.php
+++ src/SkyServiceProvider.php
@@ -64,6 +64,9 @@ class SkyServiceProvider extends PackageServiceProvider
 
     private function bootFilamentNavigation(): void
     {
+        if (!array_key_exists('zeus-sky', app('filament')->getCurrentPanel()->getPlugins())) {
+            return;
+        }
         NavigationResource::navigationGroup(SkyPlugin::get()->getNavigationGroupLabel());
         NavigationResource::navigationSort(999);
 

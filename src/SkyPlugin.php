3841b82e7a2a8e8352ddb080276b38f13f1857fd
Ash Monsh <wh7r.com@gmail.com>
2023-08-04 17:05:17 +0300

    add filament navigation

--- src/SkyPlugin.php
+++ src/SkyPlugin.php
@@ -3,12 +3,14 @@
 namespace LaraZeus\Sky;
 
 use Filament\Contracts\Plugin;
+use Filament\Forms\Components\Select;
 use Filament\Panel;
 use LaraZeus\Sky\Filament\Resources\FaqResource;
 use LaraZeus\Sky\Filament\Resources\LibraryResource;
 use LaraZeus\Sky\Filament\Resources\PageResource;
 use LaraZeus\Sky\Filament\Resources\PostResource;
 use LaraZeus\Sky\Filament\Resources\TagResource;
+use RyanChandler\FilamentNavigation\FilamentNavigation;
 
 class SkyPlugin implements Plugin
 {
@@ -38,6 +40,34 @@ class SkyPlugin implements Plugin
         }
 
         $panel->resources([TagResource::class]);
+
+        $panel->plugin(
+            FilamentNavigation::make()
+                ->itemType(__('Post link'), [
+                    Select::make('post_id')
+                        ->label(__('Select Post'))
+                        ->searchable()
+                        ->options(function () {
+                            return SkyPlugin::get()->getPostModel()::published()->pluck('title', 'id');
+                        }),
+                ])
+                ->itemType(__('Page link'), [
+                    Select::make('page_id')
+                        ->label(__('Select Page'))
+                        ->searchable()
+                        ->options(function () {
+                            return SkyPlugin::get()->getPostModel()::page()->pluck('title', 'id');
+                        }),
+                ])
+                ->itemType(__('Library link'), [
+                    Select::make('library_id')
+                        ->label(__('Select Library'))
+                        ->searchable()
+                        ->options(function () {
+                            return SkyPlugin::get()->getTagModel()::getWithType('library')->pluck('name', 'id');
+                        }),
+                ])
+        );
     }
 
     public static function make(): static
@@ -45,7 +75,7 @@ class SkyPlugin implements Plugin
         return app(static::class);
     }
 
-    public static function get(): Plugin | \Filament\FilamentManager
+    public static function get(): Plugin|\Filament\FilamentManager
     {
         return filament(app(static::class)->getId());
     }
--- src/SkyServiceProvider.php
+++ src/SkyServiceProvider.php
@@ -3,11 +3,9 @@
 namespace LaraZeus\Sky;
 
 use Filament\Facades\Filament;
-use Filament\Forms\Components\Select;
 use LaraZeus\Core\CoreServiceProvider;
 use LaraZeus\Sky\Console\migrateCommand;
 use LaraZeus\Sky\Console\PublishCommand;
-use RyanChandler\FilamentNavigation\Facades\FilamentNavigation;
 use RyanChandler\FilamentNavigation\Filament\Resources\NavigationResource;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -62,43 +60,15 @@ class SkyServiceProvider extends PackageServiceProvider
 
     private function bootFilamentNavigation(): void
     {
-        if (! array_key_exists('zeus-sky', app('filament')->getCurrentPanel()->getPlugins())) {
+        if (!app('filament')->hasPlugin('zeus-sky')) {
             return;
         }
+
         NavigationResource::navigationGroup(SkyPlugin::get()->getNavigationGroupLabel());
         NavigationResource::navigationSort(999);
-
+        NavigationResource::navigationIcon('heroicon-o-queue-list');
         NavigationResource::navigationLabel(__('Navigations'));
         NavigationResource::pluralLabel(__('Navigations'));
         NavigationResource::label(__('Navigation'));
-
-        NavigationResource::navigationIcon('heroicon-o-queue-list');
-
-        FilamentNavigation::addItemType(__('Post link'), [
-            Select::make('post_id')
-                ->label(__('Select Post'))
-                ->searchable()
-                ->options(function () {
-                    return SkyPlugin::get()->getPostModel()::published()->pluck('title', 'id');
-                }),
-        ]);
-
-        FilamentNavigation::addItemType(__('Page link'), [
-            Select::make('page_id')
-                ->label(__('Select Page'))
-                ->searchable()
-                ->options(function () {
-                    return SkyPlugin::get()->getPostModel()::page()->pluck('title', 'id');
-                }),
-        ]);
-
-        FilamentNavigation::addItemType(__('Library link'), [
-            Select::make('library_id')
-                ->label(__('Select Library'))
-                ->searchable()
-                ->options(function () {
-                    return SkyPlugin::get()->getTagModel()::getWithType('library')->pluck('name', 'id');
-                }),
-        ]);
     }
 }

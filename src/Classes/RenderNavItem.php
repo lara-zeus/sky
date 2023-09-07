2188a19e1743183e60b69b135d12fdc9baa550ba
Ash Monsh <wh7r.com@gmail.com>
2023-09-07 19:00:01 +0300

    update Render Nav Item class to include libraries items

--- src/Classes/RenderNavItem.php
+++ src/Classes/RenderNavItem.php
@@ -8,30 +8,45 @@ class RenderNavItem
 {
     public static function render(array $item, string $class = ''): string
     {
-        if ($item['type'] === 'page-link') {
+        $color = 'border-b border-b-secondary-500 text-secondary-500';
+
+        if ($item['type'] === 'page-link' || $item['type'] === 'page_link') {
             $page = SkyPlugin::get()->getPostModel()::page()->find($item['data']['page_id']) ?? '';
+            $activeClass = (request()->routeIs('page', $page)) ? $color : 'border-transparent';
 
-            return '<a class="' . $class . '"
-                    target="' . ($item['data']['target'] ?? '_self') . '"
-                    href="' . route('page', $page) . '"
-                >' .
-                $item['label'] .
+            return '<a class="'.$class.' '.$activeClass.'"
+                    target="'.($item['data']['target'] ?? '_self').'"
+                    href="'.route('page', $page).'"
+                >'.
+                $item['label'].
                 '</a>';
-        } elseif ($item['type'] === 'post-link') {
+        } elseif ($item['type'] === 'post-link' || $item['type'] === 'post_link') {
             $post = SkyPlugin::get()->getPostModel()::find($item['data']['post_id']) ?? '';
+            $activeClass = (request()->routeIs('post', $post)) ? $color : 'border-transparent';
 
-            return '<a class="' . $class . '"
-                    target="' . ($item['data']['target'] ?? '_self') . '"
-                    href="' . route('post', $post) . '"
-                >' .
-                $item['label'] .
+            return '<a class="'.$class.' '.$activeClass.'"
+                    target="'.($item['data']['target'] ?? '_self').'"
+                    href="'.route('post', $post).'"
+                >'.
+                $item['label'].
                 '</a>';
+        } elseif ($item['type'] === 'library-link' || $item['type'] === 'library_link') {
+            $tag = SkyPlugin::get()->getTagModel()::find($item['data']['library_id']) ?? '';
+            $activeClass = (str(request()->url())->contains($tag->library->first()->slug)) ? $color : 'border-transparent';
+
+            return '<a class="'.$class.' '.$activeClass.'"
+                    target="'.($item['data']['target'] ?? '_self').'"
+                    href="'.route('library.tag', $tag->slug).'"
+                >'.
+                $item['label'].
+                '</a>';
+
         } else {
-            return '<a class="' . $class . '"
-                    target="' . ($item['data']['target'] ?? '_self') . '"
-                    href="' . optional($item['data'])['url'] . '"
-                >' .
-                $item['label'] .
+            return '<a class="'.$class.'"
+                    target="'.($item['data']['target'] ?? '_self').'"
+                    href="'.$item['data']['url'].'"
+                >'.
+                $item['label'].
                 '</a>';
         }
     }

37bd19cbee7dedbf13fd98b0528bb5cd35a64c19
Ash Monsh <wh7r.com@gmail.com>
2023-09-12 14:51:26 +0300

    Create RichEditorEditor.php

---
+++ src/Editors/RichEditorEditor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LaraZeus\Sky\Editors;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\RichEditor as RichEditorAlias;
+use Filament\Forms\Components\Textarea;
+use LaraZeus\Sky\Classes\ContentEditor;
+
+class RichEditorEditor implements ContentEditor
+{
+    public static function component(): Component
+    {
+        if (class_exists(RichEditorAlias::class)) {
+            return RichEditorAlias::make('content')
+                ->required();
+        }
+
+        return Textarea::make('content')->required();
+    }
+
+    public static function render(string $content): string
+    {
+        return html_entity_decode($content);
+    }
+}
